ff23d9e2306ec72b56dd3e0f371aa7d8940dd29e
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-03-18 14:29:46 +0200

    api js ids_directory - function cargo, cargo_group, cargo_etsng shared - вынес общие функции

---
+++ js/api/ids_directory.js
@@ -0,0 +1,215 @@
+﻿(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    // Определим язык
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+            /*            'mess_load_reference': 'Загружаю справочники...',*/
+        },
+        'en':  //default language: English
+        {
+            /*            'mess_load_reference': 'Loading references ...',*/
+        }
+    };
+    // Определлим список текста для этого модуля
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang));
+
+    // Модуль инициализаии компонентов формы
+    var API_COMMON = App.api_common;
+
+    //****************************************************************************************
+    //-------------------------------- Конструктор и инициализация ---------------
+    // создать класс справочники ИДС
+    function ids_directory(options) {
+
+        //this.list_cargo = null;
+        //this.list_cargo_group = null;
+        //this.list_cargo_etsng = null;
+
+        this.settings = $.extend({
+            url_api: null,
+        }, options);
+        this.api_com = new API_COMMON({
+            list_load: [
+                { name: 'cargo', list: null, fn_get: this.getCargo },
+                { name: 'cargo_group', list: null, fn_get: this.getCargoGroup },
+                { name: 'cargo_etsng', list: null, fn_get: this.getCargoETSNG },
+            ],
+            url_api: this.settings.url_api
+        });
+    }
+    ids_directory.prototype.load = function (list, lock, update, callback) {
+        this.api_com.load(list, lock, update, callback);
+    };
+
+    //****************************************************************************************
+    //-------------------------------- Функции работы с БД через api ---------------
+    //======= Directory_Cargo (Справочник грузов) ======================================
+    //
+    ids_directory.prototype.getCargo = function (callback) {
+        $.ajax({
+            type: 'GET',
+            url: this.settings.url_api + '/DirectoryCargo/list',
+            async: true,
+            dataType: 'json',
+            beforeSend: function () {
+                AJAXBeforeSend();
+            },
+            success: function (data) {
+                if (typeof callback === 'function') {
+                    callback(data);
+                }
+            },
+            error: function (x, y, z) {
+                OnAJAXError("ids_directory.getCargo", x, y, z);
+            },
+            complete: function () {
+                AJAXComplete();
+            },
+        });
+    };
+    //======= Directory_CargoGroup (Справочник группа грузов) ======================================
+    //
+    ids_directory.prototype.getCargoGroup = function (callback) {
+        $.ajax({
+            type: 'GET',
+            url: this.settings.url_api + '/DirectoryCargoGroup',
+            async: true,
+            dataType: 'json',
+            beforeSend: function () {
+                AJAXBeforeSend();
+            },
+            success: function (data) {
+                if (typeof callback === 'function') {
+                    callback(data);
+                }
+            },
+            error: function (x, y, z) {
+                OnAJAXError("ids_directory.getCargoGroup", x, y, z);
+            },
+            complete: function () {
+                AJAXComplete();
+            },
+        });
+    };
+    //======= Directory_CargoETSNG (Справочник грузов ЕТСНГ) ======================================
+    //
+    ids_directory.prototype.getCargoETSNG = function (callback) {
+        $.ajax({
+            type: 'GET',
+            url: this.settings.url_api + '/DirectoryCargoEtsng',
+            async: true,
+            dataType: 'json',
+            beforeSend: function () {
+                AJAXBeforeSend();
+            },
+            success: function (data) {
+                if (typeof callback === 'function') {
+                    callback(data);
+                }
+            },
+            error: function (x, y, z) {
+                OnAJAXError("ids_directory.getCargoETSNG", x, y, z);
+            },
+            complete: function () {
+                AJAXComplete();
+            },
+        });
+    };
+
+    //****************************************************************************************
+    //-------------------------------- функции для работы с таблицами ------------------------
+    //*======= ids_directory.list_cargo  (Справочник грузов) ======================================
+    // Получить все записи
+    ids_directory.prototype.getAllCargo = function () {
+        var obj = this.api_com.getAllObj('cargo');
+        return obj ? obj.list : null;
+    };
+    // Получить запись по id
+    ids_directory.prototype.getCargo_Of_ID = function (id) {
+        return this.api_com.getObj_Of_field('cargo', 'id', id);
+    };
+    // Получить записи по id_etsng
+    ids_directory.prototype.getCargo_Of_IDETSNG = function (id) {
+        return this.api_com.getObj_Of_field('cargo', 'idCargoEtsng', id);
+    };
+    // Получить записи по имени
+    ids_directory.prototype.getCargo_Of_Name = function (name, text) {
+        return this.api_com.getObj_Of_field('cargo', name, text);
+    };
+    // Получить списки (Value, Text, Desabled) по указоным полям
+    ids_directory.prototype.getListCargo = function (fvalue, ftext, lang, filter) {
+        return this.api_com.getListObj('cargo', fvalue, ftext, lang, filter);
+    };
+    // Получить списки (Value, Text, Desabled) по умолчанию
+    ids_directory.prototype.getListValueTextCargo = function () {
+        return this.getListCargo('id', 'cargoName', ucFirst(App.Lang));
+    };
+    //*======= ids_directory.list_cargo_group  (Справочник группы грузов) ======================================
+    // Получить все записи
+    ids_directory.prototype.getAllCargoGroup = function () {
+        var obj = this.api_com.getAllObj('cargo_group');
+        return obj ? obj.list : null;
+    };
+    // Получить запись по id
+    ids_directory.prototype.getCargoGroup_Of_ID = function (id) {
+        return this.api_com.getObj_Of_field('cargo_group', 'id', id);
+    };
+    // Получить записи по имени
+    ids_directory.prototype.getCargoGroup_Of_Name = function (name, text) {
+        return this.api_com.getObj_Of_field('cargo_group', name, text);
+    };
+    // Получить списки (Value, Text, Desabled) по указоным полям
+    ids_directory.prototype.getListCargoGroup = function (fvalue, ftext, lang, filter) {
+        return this.api_com.getListObj('cargo_group', fvalue, ftext, lang, filter);
+    };
+    // Получить списки (Value, Text, Desabled) по умолчанию
+    ids_directory.prototype.getListValueTextCargoGroup = function () {
+        return this.getListCargoGroup('id', 'cargoGroupName', ucFirst(App.Lang));
+    };
+    //*======= ids_directory.list_cargo_etsng  (Справочник грузов ЕТСНГ) ======================================
+    // Получить все записи
+    ids_directory.prototype.getAllCargoETSNG = function () {
+        var obj = this.api_com.getAllObj('cargo_etsng');
+        return obj ? obj.list : null;
+    };
+    // Получить запись по id
+    ids_directory.prototype.getCargoETSNG_Of_ID = function (id) {
+        return this.api_com.getObj_Of_field('cargo_etsng', 'id', id);
+    };
+    // Получить записи по имени
+    ids_directory.prototype.getCargoETSNG_Of_Name = function (name, text) {
+        return this.api_com.getObj_Of_field('cargo_etsng', name, text);
+    };
+    // Получить списки (Value, Text, Desabled) по указоным полям
+    ids_directory.prototype.getListCargoETSNG = function (fvalue, ftext, lang, filter) {
+        return this.api_com.getListObj('cargo_etsng', fvalue, ftext, lang, filter);
+    };
+    // Получить списки (Value, Text1+Text2, Desabled) по указоным полям
+    ids_directory.prototype.getListCargoETSNG2 = function (fvalue, ftext1, ftext2, lang, filter) {
+        return this.api_com.getListObj2('cargo_etsng', fvalue, ftext1, ftext2, lang, filter);
+    };
+    // Получить списки (Value, Text, Desabled) по умолчанию
+    ids_directory.prototype.getListValueTextCargoETSNG = function () {
+        return this.getListCargoETSNG('id', 'cargoEtsngName', ucFirst(App.Lang));
+    };
+    // Получим первую строку в указаным поле которых есть текст text (проверка убирает все пробелы и выравнивает буквы)
+    //ids_directory.prototype.getCargoETSNG_Of_Name_find = function (name, text, lang) {
+    //    if (this.list_cargo_etsng) {
+    //        return this.getObjs_Of_text_find(this.list_cargo_etsng, name, text, lang);
+    //    }
+    //    return null;
+    //};
+
+    App.ids_directory = ids_directory;
+
+    window.App = App;
+
+})(window);
--- js/api/shared.js
+++ js/api/shared.js
@@ -30,3 +30,143 @@ var OnAJAXError = function (metod, x, y, z, callback) {
 var AJAXComplete = function () {
     //LockScreenOff();
 };
+
+(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    // Определим язык
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+    /* ----------------------------------------------------------
+                        Список слов
+    -------------------------------------------------------------*/
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+            'mess_delay': 'Мы обрабатываем ваш запрос...',
+        },
+        'en':  //default language: English
+        {
+            'mess_delay': 'We are processing your request ...',
+        }
+
+    };
+    // Определлим список текста для этого модуля
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang));
+
+    //================================================================================
+    // Класс для создания объектов 
+    //--------------------------------Конструктор и инициализация---------------
+    // создать класс
+    function api_common(options) {
+        this.settings = $.extend({
+            list_load: [],
+            url_api: null,
+        }, options);
+    };
+
+    api_common.prototype.load = function (list, lock, update, callback) {
+        var process = 0;
+        var result = [];
+        //{ name='cargo', list=this.list_cargo, fn_get=this.getCargo }
+        var out_load = function (process) {
+            if (process === 0) {
+                if (lock) { LockScreenOff(); }
+                if (typeof callback === 'function') {
+                    callback(result);
+                }
+            }
+        };
+        if (list) {
+            $.each(list, function (i, table) {
+                $.each(this.settings.list_load, function (i, set_load) {
+                    if (table === set_load.name) {
+                        if (lock) LockScreen(langView('mess_load_reference', App.Langs));
+                        if (update || !set_load.list) {
+                            process++;
+                            set_load.fn_get.call(this, function (data) {
+                                set_load.list = data;
+                                process--;
+                                result.push(set_load.name);
+                                out_load(process);
+                            }.bind(this))
+                        };
+                    };
+                }.bind(this));
+            }.bind(this));
+        };
+        out_load(process);
+    };
+
+    //****************************************************************************************
+    //-------------------------------- функции для работы с объектами ------------------------
+    // Показать весь список
+    api_common.prototype.getAllObj = function (name_list_obj) {
+        return this.settings.list_load.find(function (o) { return o.name === name_list_obj })
+    };
+    // выбрать из списка по полю
+    api_common.prototype.getObj_Of_field = function (name_list_obj, field_name, value) {
+        var obj = null;
+        //var res = this.settings.list_load.find(function (o) { return o.name === name_list_obj });
+        var res = this.getAllObj(name_list_obj);
+        if (res) {
+            if (res.list && res.list.length > 0) {
+                obj = res.list.find(function (o) { return $.trim(o[field_name]) == $.trim(value) });
+            }
+        }
+        return obj;
+    };
+    // Вернуть спсисок объектов таблицы в формате {value:, text:}
+    api_common.prototype.getListObj = function (name_list_obj, fvalue, ftext, lang, filter) {
+        var list_obj = this.getAllObj(name_list_obj);
+        var list = [];
+        var list_filtr = null;
+        if (list_obj && list_obj.list) {
+            if (typeof filter === 'function') {
+                list_filtr = list_obj.list.filter(filter);
+            } else { list_filtr = list_obj.list; }
+            $.each(list_filtr, function (i, el) {
+                if (lang) {
+                    list.push({ value: el[fvalue], text: el[ftext + lang], disabled: false });
+                } else {
+                    list.push({ value: el[fvalue], text: el[ftext], disabled: false });
+                }
+            }.bind(this));
+        }
+        return list;
+    };
+    // Вернуть спсисок объектов таблицы в формате {value:, text:}
+    api_common.prototype.getListObj2 = function (name_list_obj, fvalue, ftext1, ftext2, lang, filter) {
+        var list_obj = this.getAllObj(name_list_obj);
+        var list = [];
+        var list_filtr = null;
+        if (list_obj && list_obj.list) {
+            if (typeof filter === 'function') {
+                list_filtr = list_obj.list.filter(filter);
+            } else { list_filtr = list_obj.list; }
+            for (var i = 0, j = list_filtr.length; i < j; i++) {
+                var l = list_filtr[i];
+                if (lang) {
+                    list.push({ value: l[fvalue], text: l[ftext1 + lang] + ' - ' + l[ftext2 + lang], disabled: false });
+                } else {
+                    list.push({ value: l[fvalue], text: l[ftext1] + ' - ' + l[ftext2], disabled: false });
+                }
+            }
+        }
+        return list;
+    };
+    //// Вернуть строку с первой заглавной
+    //api_common.prototype.ucFirst = function (str) {
+    //    if (!str) return str;
+    //    return str[0].toUpperCase() + str.slice(1);
+    //};
+
+    App.api_common = api_common;
+
+    window.App = App;
+
+})(window);
