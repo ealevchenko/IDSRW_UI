e85e05b17252347fc0c2804142bc06f12db96c82
Levchenko <eduard.levchenko@arcelormittal.com>
2024-12-17 11:23:15 +0200

    Разделил таблицы вагоны на пути погрузка выгрузка Обновил запреты погрузка разгрузка

--- js/module/ws/table_ws.js
+++ js/module/ws/table_ws.js
@@ -2303,7 +2303,46 @@
         collums.push({ field: 'arrival_division_amkr_abbr', title: null, class: null });
         return this.tab_com.init_columns_detali(collums, this.tab_com.list_collums);
     };
-    table_ws.prototype.init_columns_unload_cars_from = function () {
+
+    table_ws.prototype.init_columns_unload_cars_from1 = function () {
+        var collums = [];
+        collums.push({ field: 'position', title: null, class: null });
+        if (this.tab_com.settings.link_num) {
+            collums.push({ field: 'num_link', title: null, class: null });
+        } else {
+            collums.push({ field: 'num', title: null, class: null });
+        }
+        /*        collums.push({ field: 'outgoing_sostav_status_name', title: null, class: null });*/
+        collums.push({ field: 'current_wagon_busy', title: null, class: null });
+        collums.push({ field: 'arrival_nom_main_doc', title: null, class: null });
+        collums.push({ field: 'arrival_nom_doc', title: null, class: null });
+        // Номер путевой
+        collums.push({ field: 'wagon_rod_abbr', title: null, class: null });
+        collums.push({ field: 'wagon_adm', title: null, class: null });
+        //collums.push({ field: 'arrival_condition_abbr', title: null, class: null });
+        collums.push({ field: 'current_condition_abbr', title: null, class: null });
+        collums.push({ field: 'operator_abbr', title: null, class: 'operator' });
+        collums.push({ field: 'limiting_abbr', title: null, class: null });
+        collums.push({ field: 'current_loading_status', title: null, class: null });
+        collums.push({ field: 'arrival_cargo_group_name', title: null, class: null });
+        collums.push({ field: 'arrival_cargo_name', title: null, class: null });
+        collums.push({ field: 'arrival_sertification_data', title: null, class: null });
+        // Станция отправления ТЕКУЩАЯ
+        // Станция назначения ТЕКУЩАЯ
+        // Цех погрузки ТЕКУЩАЯ
+        // Цех получатель ТЕКУЩАЯ
+        collums.push({ field: 'arrival_division_amkr_abbr', title: null, class: null });
+        collums.push({ field: 'current_operation_name', title: null, class: null });
+        collums.push({ field: 'current_operation_start', title: null, class: null });
+        collums.push({ field: 'current_operation_end', title: null, class: null });
+        //collums.push({ field: 'id_filing', title: null, class: null });
+        //collums.push({ field: 'start_filing', title: null, class: null });
+        //collums.push({ field: 'end_filing', title: null, class: null });
+        //collums.push({ field: 'way_filing_start', title: null, class: null });
+        //collums.push({ field: 'way_filing_end', title: null, class: null });
+        return this.tab_com.init_columns_detali(collums, this.tab_com.list_collums);
+    };
+    table_ws.prototype.init_columns_unload_cars_from2 = function () {
         var collums = [];
         collums.push({ field: 'position', title: null, class: null });
         if (this.tab_com.settings.link_num) {
@@ -3226,8 +3265,8 @@
                 this.tab_com.dom = 'Bfrtip';
                 break;
             };
-            // Вагоны на пути отправки
-            case 'unload_cars_from': {
+            // Вагоны на пути отправки выгрузка
+            case 'unload_cars_from_1': {
                 this.tab_com.lengthMenu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, langView('t_com_title_all', App.Langs)]];
                 this.tab_com.pageLength = 10;
                 this.tab_com.deferRender = true;
@@ -3264,7 +3303,7 @@
                     //        $('td', row).eq(1).addClass('idle-time-error');
                     //    };
                     //}
-                    if (data.id_wir_unload !== null || data.currentWagonBusy || data.outgoingSostavStatus !== null) // || (data.startFiling !== null && data.endFiling === null)
+                    if (data.id_wir_unload !== null || data.currentUnloadBusy || data.currentWagonBusy || data.outgoingSostavStatus !== null) // || (data.startFiling !== null && data.endFiling === null)
                     {
                         $(row).addClass('ban red');  // Отметим вагон заблокирован
                     }
@@ -3286,7 +3325,72 @@
                         }
                     }
                 }.bind(this);
-                this.tab_com.table_columns = this.init_columns_unload_cars_from();
+                this.tab_com.table_columns = this.init_columns_unload_cars_from1();
+                this.tab_com.table_buttons = this.tab_com.init_button_Ex_Prn_Fld_Ref_EyE_Pag(this.tab_com.settings.setup_buttons);
+                this.tab_com.dom = 'Bfrtip';
+                break;
+            };
+            // Вагоны на пути отправки погрузка
+            case 'unload_cars_from_2': {
+                this.tab_com.lengthMenu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, langView('t_com_title_all', App.Langs)]];
+                this.tab_com.pageLength = 10;
+                this.tab_com.deferRender = true;
+                this.tab_com.paging = true;
+                this.tab_com.searching = true;
+                this.tab_com.ordering = true;
+                this.tab_com.info = true;
+                this.tab_com.fixedHeader = true;            // вкл. фикс. заголовка
+                this.tab_com.leftColumns = 2;
+                this.tab_com.columnDefs = null;
+                this.tab_com.order_column = [0, 'asc'];
+                this.tab_com.type_select_rows = 2; // Выбирать одну
+                this.tab_com.table_select = {
+                    style: 'multi'
+                };
+                this.tab_com.autoWidth = true;
+                this.tab_com.createdRow = function (row, data, index) {
+                    $(row).attr('id', data.fromIdWim); // id строки дислокации вагона в момент отправки
+                    $(row).attr('data-num', data.num); // data-num номер вагона
+                    if (data.wirHighlightColor !== null) {
+                        $(row).attr('style', 'background-color:' + data.wirHighlightColor + ' !important;');
+                    }
+                    // Цвет оператора
+                    if (data.operatorColor && data.operatorColor !== '') {
+                        $('td', row).eq(12).attr('style', 'background-color:' + data.operatorColor)
+                        //$('td.operator', row).attr('style', 'background-color:' + data.operatorColor)
+                    }
+                    //// Проверим если по оператору контролировать норму времени, тогда проверить
+                    //if (data.arrivalIdleTime < data.arrivalDuration) {
+                    //    // Превышена норма нахождения вагона на АМКР
+                    //    $('td', row).eq(29).addClass('idle-time-error');
+                    //    //$('td.arrival-duration', row).addClass('idle-time-error');
+                    //    if (data.operatorMonitoringIdleTime) {
+                    //        $('td', row).eq(1).addClass('idle-time-error');
+                    //    };
+                    //}
+                    if (data.id_wir_unload !== null || data.currentLoadBusy || data.currentWagonBusy || data.outgoingSostavStatus !== null) // || (data.startFiling !== null && data.endFiling === null)
+                    {
+                        $(row).addClass('ban red');  // Отметим вагон заблокирован
+                    }
+                    // Прибыл
+                    if (data.currentIdOperation === 1) {
+                        //$('td.fixed-column', row).addClass('red'); // Отметим прибытие
+                        $('td', row).eq(0).addClass('red');
+                        $('td', row).eq(1).addClass('red');
+                    }
+                    // Предъявлен или сдан
+                    if (data.currentIdOperation === 9 || data.currentIdOperation === 8) {
+                        if (data.outgoingSostavStatus === 2) {
+                            $('td', row).eq(0).addClass('green');
+                            $('td', row).eq(1).addClass('green');
+                        }
+                        if (data.outgoingSostavStatus === 1 || data.outgoingSostavStatus === 0) {
+                            $('td', row).eq(0).addClass('yellow');
+                            $('td', row).eq(1).addClass('yellow');
+                        }
+                    }
+                }.bind(this);
+                this.tab_com.table_columns = this.init_columns_unload_cars_from2();
                 this.tab_com.table_buttons = this.tab_com.init_button_Ex_Prn_Fld_Ref_EyE_Pag(this.tab_com.settings.setup_buttons);
                 this.tab_com.dom = 'Bfrtip';
                 break;
