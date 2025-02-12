d4b7ba774661f265e990602b2b9c750d467d7f28
Levchenko <eduard.levchenko@arcelormittal.com>
2025-02-12 16:43:40 +0200

    Добавил страницу вызова отчетов для печати

---
+++ js/module/ws/view_incoming_report.js
@@ -0,0 +1,68 @@
+﻿/* ===============================================
+-= Модуль документы по прибытию =-
+  + js/view/shared/common.js
+  + js/module/view_op_common.js
+  + js/module/ws/table_ws.js
+==================================================*/
+(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    // Определим язык
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+            'vicr_mess_init_module': 'Инициализация модуля(view_incoming_report)...',
+        },
+        'en':  //default language: English
+        {
+        }
+    };
+    // Определлим список текста для этого модуля
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang));
+
+    // js/module/view_op_common.js
+    var VIEW_COMMON = App.view_op_common;
+    // js/view/shared/common.js
+
+    var ALERT = App.alert_form;
+    var FD = App.form_dialog;
+    // js/module/ws/table_ws.js
+    var TWS = App.table_ws;
+
+    function view_incoming_report() {
+
+    }
+    // инициализация модуля
+    view_incoming_report.prototype.init = function (options) {
+        LockScreen(langView('vicr_mess_init_module', App.Langs));
+        // теперь выполним инициализацию, определим основные свойства
+        this.settings = $.extend({
+            api_dir: null,
+            api_wsd: null,
+            fn_init: null,
+        }, options);
+        // Инициализация вначале 
+        if (typeof this.settings.fn_init === 'function') {
+            this.settings.fn_init.call(this, true);
+        }
+    };
+    // Показать данные 
+    view_incoming_report.prototype.view = function (id) {
+        //LockScreen(langView('vopac_mess_load_operation', App.Langs));
+    };
+    // Выбрать все вагоны выбранного состава 
+    view_incoming_report.prototype.destroy = function () {
+
+    };
+
+    App.view_incoming_report = view_incoming_report;
+
+    window.App = App;
+
+})(window);
---
+++ js/view/print/main_print.js
@@ -0,0 +1,44 @@
+﻿(function ($) {
+    "use strict"; // Start of use strict
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+
+            //'title_select': 'Выберите...',
+
+        },
+        'en':  //default language: English
+        {
+            //'title_select': 'Выберите...',
+        }
+    };
+
+    // Определим глобальные переменные
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang)); //
+    App.User_Name = $('input#username').val();
+
+    var VICR = App.view_incoming_report;
+    var view_incoming_report = new VICR();
+
+    // Считаем строку с дополнительными параметрами
+    var report = getUrlVar('report');
+    var id = getUrlVar('id');
+
+    $(document).ready(function ($) {
+        // Инициализация модуля "Отчет принятых составов"
+        view_incoming_report.init({
+            api_dir: null,
+            api_wsd: null,
+            fn_init: function (init) {
+                view_incoming_report.view(id)
+                LockScreenOff();
+            }.bind(this),
+        });
+    });
+})(jQuery); // End of use strict
