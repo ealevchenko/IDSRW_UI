bc2228820efa8184b9b2c64056c7b7cb0a6c0307
Levchenko <eduard.levchenko@arcelormittal.com>
2025-03-25 10:17:34 +0200

    add ids_arrival

---
+++ js/api/ids_arrival.js
@@ -0,0 +1,67 @@
+﻿(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    // Определим язык
+    App.Lang = ($.cookie('lang') === undefined ? 'ru' : $.cookie('lang'));
+
+    // Массив текстовых сообщений 
+    $.Text_View =
+    {
+        'default':  //default language: ru
+        {
+            /*            'mess_load_reference': 'Загружаю справочники...',*/
+        },
+        'en':  //default language: English
+        {
+            /*            'mess_load_reference': 'Loading references ...',*/
+        }
+    };
+    // Определлим список текста для этого модуля
+    App.Langs = $.extend(true, App.Langs, getLanguages($.Text_View, App.Lang));
+
+    // Модуль инициализаии компонентов формы
+    var API_COMMON = App.api_common;
+
+    //****************************************************************************************
+    //-------------------------------- Конструктор и инициализация ---------------
+    // создать класс справочники ИДС
+    function ids_arrival(options) {
+
+        //this.list_cargo = null;
+        //this.list_cargo_group = null;
+        //this.list_cargo_etsng = null;
+
+        this.settings = $.extend({
+            url_api: null,
+        }, options);
+        this.api_com = new API_COMMON({
+            list_load: [
+
+            ],
+            url_api: this.settings.url_api
+        });
+    }
+    ids_arrival.prototype.load = function (list, lock, update, callback) {
+        this.api_com.load(list, lock, update, callback);
+    };
+
+    //****************************************************************************************
+    //-------------------------------- Функции работы с БД через api ---------------
+
+    //================= ПРИБЫТИЕ =============================================
+    // Получить документы по прибытию храящиеся в БД
+    ids_arrival.prototype.getArrivalUzDocument = function (id, callback) {
+        this.api_com.get('/ArrivalUzDocument/' + id, callback);
+    };
+    // Получить список документов (id, №Накл. дата расчета) по прибытию храящиеся в БД за период
+    ids_arrival.prototype.getListMainDocArrivalUzDocument = function (start, stop, callback) {
+        this.api_com.get('/ArrivalUzDocument/list/main_doc/start/' + start + '/stop/' + stop, callback);
+    };
+
+    App.ids_arrival = ids_arrival;
+
+    window.App = App;
+
+})(window);
