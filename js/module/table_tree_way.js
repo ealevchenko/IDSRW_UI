19f9c8beecfdedbf27fdbb4d82ffa5b5acb93d4c
Levchenko <eduard.levchenko_old@arcelormittal.com>
2026-01-20 16:34:12 +0200

    добавил обработку ошибки api_dir.getViewStatusAllStation(

--- js/module/table_tree_way.js
+++ js/module/table_tree_way.js
@@ -91,7 +91,7 @@
         }, options);
         // определим библиотеку  dir
         this.api_dir = this.settings.api_dir ? this.settings.api_dir : new API_DIRECTORY({ url_api: App.Url_Api });
-;
+        ;
         this.$tw.attr("style", "overflow-x:auto");
         this.$tw.empty();
         this.$table = new this.fe_ui.table({
@@ -347,17 +347,22 @@
         LockScreen(langView('mess_load_station', App.Langs));
         this.api_dir.getViewStatusAllStation(function (stations) {
             if (typeof callback === 'function') {
-                var current_stations = stations.filter(function (i) {
-                    return !i.station_uz;
-                });
-                if (list_station && list_station.length > 0) {
-                    current_stations = current_stations.filter(function (i) {
-                        return list_station.find(function (o) {
-                            return o.id == i.id && o.checked;
-                        });
+                if (stations) {
+                    var current_stations = stations.filter(function (i) {
+                        return !i.station_uz;
                     });
+                    if (list_station && list_station.length > 0) {
+                        current_stations = current_stations.filter(function (i) {
+                            return list_station.find(function (o) {
+                                return o.id == i.id && o.checked;
+                            });
+                        });
+                    }
+                    callback(current_stations);
+
+                } else {
+                    callback([]);
                 }
-                callback(current_stations);
             }
         }.bind(this));
     };
